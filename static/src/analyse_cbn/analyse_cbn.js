fcf1e00ee1f77d3d35db7b5b80e6e8ed5fde3311
Tony Galmiche <tony.galmiche@infosaone.com>
2023-04-25 15:50:28 +0200

    modifs

--- static/src/analyse_cbn/analyse_cbn.js
+++ static/src/analyse_cbn/analyse_cbn.js
@@ -1,5 +1,4 @@
 /** @odoo-module **/
-import { AnalyseCbnTr } from "../analyse_cbn/analyse_cbn_tr";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { getDefaultConfig } from "@web/views/view";
@@ -12,9 +11,21 @@ class AnalyseCbn extends Component {
         this.user_id = useService("user").context.uid;
         this.orm     = useService("orm");
         this.state   = useState({
-            'analyse_cbn_codepg': false,
+            'analyse_cbn_code_pg': false,
+            'analyse_cbn_gest': false,
+            'analyse_cbn_cat': false,
+            'analyse_cbn_moule': false,
+            'analyse_cbn_projet': false,
+            'analyse_cbn_client': false,
+            'analyse_cbn_fournisseur': false,
+            'analyse_cbn_semaines': false,
+            'analyse_cbn_type_cde': false,
+            'analyse_cbn_type_rapport': false,
+            'analyse_cbn_calage': false,
+            'analyse_cbn_val': false,
             'lines': [],
         });
+
         useSubEnv({
             config: {
                 ...getDefaultConfig(),
@@ -25,7 +36,18 @@ class AnalyseCbn extends Component {
             controlPanel: { "top-right": false, "bottom-right": false },
         };
         onWillStart(async () => {
-            this.state.analyse_cbn_codepg = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_codepg"]);
+            this.state.analyse_cbn_code_pg = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_code_pg"]);
+            this.state.analyse_cbn_gest = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_gest"]);
+            this.state.analyse_cbn_cat = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_cat"]);
+            this.state.analyse_cbn_moule = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_moule"]);
+            this.state.analyse_cbn_projet = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_projet"]);
+            this.state.analyse_cbn_client = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_client"]);
+            this.state.analyse_cbn_fournisseur = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_fournisseur"]);
+            this.state.analyse_cbn_semaines = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_semaines"]);
+            this.state.analyse_cbn_type_cde = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_type_cde"]);
+            this.state.analyse_cbn_type_rapport = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_type_rapport"]);
+            this.state.analyse_cbn_calage = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_calage"]);
+            this.state.analyse_cbn_val = await this.orm.call("is.mem.var", 'get', [false, this.user_id, "analyse_cbn_val"]);
             this.getAnalyseCbn();
         });
     } 
@@ -52,8 +74,20 @@ class AnalyseCbn extends Component {
     async getAnalyseCbn(){
         var lines = await this.orm.call("product.product", 'get_analyse_cbn', [
             false,
-            this.state.analyse_cbn_codepg,
+            this.state.analyse_cbn_code_pg,
+            this.state.analyse_cbn_gest,
+            this.state.analyse_cbn_cat,
+            this.state.analyse_cbn_moule,
+            this.state.analyse_cbn_projet,
+            this.state.analyse_cbn_client,
+            this.state.analyse_cbn_fournisseur,
+            this.state.analyse_cbn_semaines,
+            this.state.analyse_cbn_type_cde,
+            this.state.analyse_cbn_type_rapport,
+            this.state.analyse_cbn_calage,
+            this.state.analyse_cbn_val,
         ]);
+
         this.state.lines=lines;
         console.log("lines=",lines);
     }
@@ -62,10 +96,7 @@ class AnalyseCbn extends Component {
 
 
 
-AnalyseCbn.components = {
-    Layout,
-    AnalyseCbnTr,
-};
+AnalyseCbn.components = { Layout };
 AnalyseCbn.template = "is_plastigray16.analyse_cbn_template";
 registry.category("actions").add("is_plastigray16.analyse_cbn_registry", AnalyseCbn);
 
