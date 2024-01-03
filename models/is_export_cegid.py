b80218b12d73e02d7e16952a4749f5a105e78945
Tony Galmiche <tony.galmiche@infosaone.com>
2024-01-03 11:17:43 +0100

    modifs

--- models/is_export_cegid.py
+++ models/is_export_cegid.py
@@ -480,9 +480,9 @@ class is_export_cegid(models.Model):
 
             company  = self.env.user.company_id
             dossier_interface_cegid = company.is_dossier_interface_cegid
-            dossier_interface_cegid = 'root@freedom:"/Utilisateurs/Interface\\ CEGID/Odoo16/"'
+            #dossier_interface_cegid = 'root@freedom:"/Utilisateurs/Interface\\ CEGID/Odoo16/"'
 
-            print("dossier_interface_cegid=",dossier_interface_cegid)
+            #print("dossier_interface_cegid=",dossier_interface_cegid)
 
 
             if dossier_interface_cegid:
