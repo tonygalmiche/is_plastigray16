2baf492e7625d6fd5b5b4af060c6040e2e6b8486
Tony Galmiche <tony.galmiche@infosaone.com>
2024-09-24 12:27:58 +0200

    modifs

--- models/res_partner.py
+++ models/res_partner.py
@@ -504,7 +504,43 @@ class res_partner(models.Model):
         return
 
 
+    #TODO : Fonction de base remplacée le 24/09/2024 car val=None
+    @api.depends_context('company')
+    def _credit_debit_get(self):
+        tables, where_clause, where_params = self.env['account.move.line']._where_calc([
+            ('parent_state', '=', 'posted'),
+            ('company_id', '=', self.env.company.id)
+        ]).get_sql()
 
+        where_params = [tuple(self.ids)] + where_params
+        if where_clause:
+            where_clause = 'AND ' + where_clause
+        self._cr.execute("""SELECT account_move_line.partner_id, a.account_type, SUM(account_move_line.amount_residual)
+                      FROM """ + tables + """
+                      LEFT JOIN account_account a ON (account_move_line.account_id=a.id)
+                      WHERE a.account_type IN ('asset_receivable','liability_payable')
+                      AND account_move_line.partner_id IN %s
+                      AND account_move_line.reconciled IS NOT TRUE
+                      """ + where_clause + """
+                      GROUP BY account_move_line.partner_id, a.account_type
+                      """, where_params)
+        treated = self.browse()
+        for pid, type, val in self._cr.fetchall():
+            if val:
+                partner = self.browse(pid)
+                if type == 'asset_receivable':
+                    partner.credit = val
+                    if partner not in treated:
+                        partner.debit = False
+                        treated |= partner
+                elif type == 'liability_payable':
+                    partner.debit = -val
+                    if partner not in treated:
+                        partner.credit = False
+                        treated |= partner
+        remaining = (self - treated)
+        remaining.debit = False
+        remaining.credit = False
 
 
     @api.constrains('vat', 'country_id')
