3e1933ce15b140e216e05ffecdc304316d901f37
Tony Galmiche <tony.galmiche@infosaone.com>
2025-08-30 10:39:51 +0200

    modifs

--- models/is_facture_proforma_export_tunisie.py
+++ models/is_facture_proforma_export_tunisie.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class IsFactureProformaExportTunisie(models.Model):
 	_name = 'is.facture.proforma.export.tunisie'
 	_description = 'Facture Proforma Export Tunisie'
 	_order = 'chrono desc'
+	_inherit = ['mail.thread', 'mail.activity.mixin']
 
-	chrono             = fields.Integer('Chrono', index=True)
-	client_id          = fields.Many2one('res.partner', 'Client')
-	adresse            = fields.Text('Adresse de facturation')
-	date_creation      = fields.Date("Date de création", default=lambda self: fields.Datetime.now(), readonly=True)
+	chrono             = fields.Integer('Chrono', index=True, tracking=True, readonly=True, copy=False)
+	client_id          = fields.Many2one('res.partner', 'Client', tracking=True, domain=[("is_company","=",True), ("customer","=",True)])
+	adresse            = fields.Text('Adresse de facturation', tracking=True)
+	date_creation      = fields.Date("Date de création", default=lambda self: fields.Datetime.now(), readonly=True, tracking=True)
 	type_exportation   = fields.Selection([
 		('10', 'Exportation définitive'),
 		('20', 'Exportation temporaire'),
 		('30', 'Retour pour perfectionnement actif réparation'),
 		('40', "Exportation définitive suite mise en conformité"),
 		('50', 'Réexportation après réparation'),
-	], "Type d'exportation")
+	], "Type d'exportation", tracking=True)
 	mode_transport     = fields.Selection([
 		('10', 'Routier'),
 		('20', 'Maritime'),
 		('30', 'Aérien'),
-	], 'Mode de transport')
-	commentaire        = fields.Text('Commentaire')
+	], 'Mode de transport', tracking=True)
+	commentaire        = fields.Text('Commentaire', tracking=True)
+	active             = fields.Boolean('Actif', default=True, tracking=True)
+	dynacase_id        = fields.Integer(string='Id Dynacase', index=True, copy=False, tracking=True)
 
 	# Lignes de colisage
 	colisage_ids       = fields.One2many(
@@ -38,6 +41,37 @@ class IsFactureProformaExportTunisie(models.Model):
 		'proforma_id',
 		'Lignes de facture'
 	)
+
+	def lien_vers_dynacase_action(self):
+		"""Ouvre la fiche Dynacase liée si un identifiant est renseigné."""
+		self.ensure_one()
+		if not self.dynacase_id:
+			return False
+		url = f"https://dynacase-rp/?sole=Y&app=FDL&action=FDL_CARD&id={int(self.dynacase_id)}"
+		return {
+			'type'  : 'ir.actions.act_url',
+			'target': 'new',
+			'url'   : url,
+		}
+
+
+	@api.model_create_multi
+	def create(self, vals_list):
+		for vals in vals_list:
+			if "chrono" not in vals:
+				last_codif = self.env["is.facture.proforma.export.tunisie"].search([('chrono', '!=', None)], order="chrono desc", limit=1)
+				if last_codif:
+					chrono = last_codif.chrono
+				else:
+					chrono = 0
+				vals["chrono"] = chrono + 1
+
+
+		print(vals_list)
+
+		return super().create(vals_list)
+
+
 
 
 class IsFactureProformaExportTunisieColisage(models.Model):
